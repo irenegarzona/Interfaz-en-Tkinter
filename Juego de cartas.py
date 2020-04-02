4c7a89ec0fad258e9db1bec2c829c44507297fce
Mariana612 <62966281+Mariana612@users.noreply.github.com>
2020-04-02 16:51:54 -0600

    Update Juego de cartas.py

--- Juego de cartas.py
+++ Juego de cartas.py
@@ -23,4 +23,10 @@ def window():
     window = Toplevel()
     window.title('Ventana B')
     window.minsize(300,200)
+    
+ventana=Tk()
+ventana.title('Memoria')
+ventana.minsize(600,400)
+ventana.resizable(width=True, height=True)
+ventana.geometry('400x400+500+150')
 
