efd42c9392e2a21102df5ee60a723dfd0c6d65c1
irenegarzona <62965324+irenegarzona@users.noreply.github.com>
2020-04-02 16:37:26 -0600

    Create Juego de cartas.py

---
+++ Juego de cartas.py
@@ -0,0 +1,18 @@
+#############################################################################
+#
+#   Instituto Tecnológico de Costa Rica
+#
+#   Área Academica de Ingeniería en Computadores
+#
+#   Curso: Taller de Programación
+#
+#   Programa: Python
+#
+#   Autor: Irene Garzona Moya
+#
+#   Versión: 3.8.1
+#
+#   Profesor:X
+#
+#   Tarea Corta: Interfaz gráfica en Tkinter
+#
