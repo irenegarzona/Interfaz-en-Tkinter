ed639230aa9011c385b2c775ddbe529b8d4c503d
irenegarzona <62965324+irenegarzona@users.noreply.github.com>
2020-04-02 16:41:16 -0600

    Update Juego de cartas.py

--- Juego de cartas.py
+++ Juego de cartas.py
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-#############################################################################
-#
-#   Instituto Tecnológico de Costa Rica
-#
-#   Área Academica de Ingeniería en Computadores
-#
-#   Curso: Taller de Programación
-#
-#   Programa: Python
-#
-#   Autor: Irene Garzona Moya
-#
-#   Versión: 3.8.1
-#
-#   Profesor:X
-#
-#   Tarea Corta: Interfaz gráfica en Tkinter
-#
-=======
 #############################################################################
 #
 #   Instituto Tecnológico de Costa Rica
@@ -37,5 +17,5 @@
 #   Tarea Corta: Interfaz gráfica en Tkinter
 #
 #############################################################################
-import Tkinter
+import Tkinter 
 >>>>>>> 291c270f0a97a700ea71efb085ac512142724cc1
